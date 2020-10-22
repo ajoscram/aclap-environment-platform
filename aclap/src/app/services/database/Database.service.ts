e6d9f6cd97925e2ed15520f711bf2dbff39c1ab0
fabioalpizar <josefabiorocks@gmail.com>
2020-10-22 11:36:06 -0600

    added quick error to database service

--- aclap/src/app/services/database/Database.service.ts
+++ aclap/src/app/services/database/Database.service.ts
@@ -2,25 +2,25 @@ import { Component, DisciplineMetadata, File, IComponent, IFile, IModule, Module
 
 export abstract class Database{
     //modules
-    getModule: (id: string) => Module;
-    geModules: () => Module[];
-    addModule: (module: IModule) => void;
-    updateModule: (id: string, module: IModule) => void;
-    deleteModule: (id: string) => void;
+    getModule: (id: string) => Promise<Module>;
+    geModules: () => Promise<Module[]>;
+    addModule: (module: IModule) => Promise<void>;
+    updateModule: (id: string, module: IModule) => Promise<void>;
+    deleteModule: (id: string) => Promise<void>;
 
     //components
-    getDisciplineMetadata: () => DisciplineMetadata;
-    getComponents: (moduleId: string) => Component[];
-    addComponent: (moduleId: string, component: IComponent) => void;
-    updateComponent: (moduleId: string, componentId: string, component: IComponent) => void;
-    deleteComponent: (moduleId: string, componentId: string) => void;
+    getDisciplineMetadata: () => Promise<DisciplineMetadata>;
+    getComponents: (moduleId: string) => Promise<Component[]>;
+    addComponent: (moduleId: string, component: IComponent) => Promise<void>;
+    updateComponent: (moduleId: string, componentId: string, component: IComponent) => Promise<void>;
+    deleteComponent: (moduleId: string, componentId: string) => Promise<void>;
 
     //files
-    getFiles: (moduleId: string, componentId: string) => File[];
-    addFile: (moduleId: string, componentId: string, file: IFile) => void;
-    deleteFile: (moduleId: string, componentId: string, fileId: string) => void;
+    getFiles: (moduleId: string, componentId: string) => Promise<File[]>;
+    addFile: (moduleId: string, componentId: string, file: IFile) => Promise<void>;
+    deleteFile: (moduleId: string, componentId: string, fileId: string) => Promise<void>;
 }
 
 export enum DatabaseError{
-
+    NOT_YET_IMPLEMENTED = "DatabaseError.NOT_YET_IMPLEMENTED"
 }
---
+++ aclap/src/app/services/database/FirebaseDatabase.service.ts
@@ -0,0 +1,35 @@
+import { Module, IModule, DisciplineMetadata, Component, IComponent, File, IFile } from '@src/app/models';
+import { Database, DatabaseError } from './Database.service';
+
+
+const firebase = require('nativescript-plugin-firebase');
+
+export class FirebaseDatabase implements Database{
+    
+    async getModule (id: string): Promise<Module>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) }
+
+    async geModules (): Promise<Module[]>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async addModule (module: IModule): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async updateModule (id: string, module: IModule): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async deleteModule (id: string): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async getDisciplineMetadata (): Promise<DisciplineMetadata>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async getComponents (moduleId: string): Promise<Component[]>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async addComponent (moduleId: string, component: IComponent): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async updateComponent (moduleId: string, componentId: string, component: IComponent): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async deleteComponent (moduleId: string, componentId: string): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async getFiles (moduleId: string, componentId: string): Promise<File[]>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async addFile (moduleId: string, componentId: string, file: IFile): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+
+    async deleteFile (moduleId: string, componentId: string, fileId: string): Promise<void>{ throw new Error(DatabaseError.NOT_YET_IMPLEMENTED) };
+    
+};
