ebd4b80d3fee2b8f2d83f93e5bd2cd02374bd617
Josue Canales <jcanales@ic-itcr.ac.cr>
2021-01-12 00:59:22 -0600

    score Assests and Major UI changes

--- aclap/src/app/components/home/home.component.ts
+++ aclap/src/app/components/home/home.component.ts
@@ -9,6 +9,8 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  initial_message = "El Programa de Educación Ambiental del Área de Conservación La Amistad Pacífico (ACLA-P), es un esfuerzo colectivo de la institución, sus funcionarios y funcionarias, en conjunto con organizaciones de base comunal, sector científico, y personas líderes comunales. Los aportes de cada una de estas personas han forjado los cimientos de esta plataforma, que ahora toma ventaja de las tecnologías de la información y los principios del libre acceso, para compartir las herramientas educativas que tenemos a disposición. Además, invitamos a los y las usuarias, a compartir con nosotros sus logros al implementar las actividades de educación ambiental en el ACLA-P. Esta plataforma es una ventana para que todas las personas que sienten el anhelo por contribuir a una cultura ambiental, puedan hacerlo desde los procesos educativos, ya sea en sus núcleos familiares, su comunidad o incluso a nivel organizacional y regional. Los procesos de educación ambiental pueden tomar formas muy variadas, desde acciones concretas en los hogares, actividades en centros educativos, hasta festivales y celebraciones cantonales o nacionales. Todos y cada uno de estos aportes suman hacia el desarrollo sostenible global. Explora los módulos educativos que tenemos para compartir, celebra las efemérides ambientales más queridas en nuestra institución, y cuéntanos como utilizas estas herramientas para el bien de todos los seres vivos de este planeta.";
+
   ngOnInit(): void {
   }
 }
--- aclap/src/app/components/profile-shared/implementation-page/implementation-page.component.ts
+++ aclap/src/app/components/profile-shared/implementation-page/implementation-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Implementation } from '@src/app/models';
+import { ActivatedRoute } from '@angular/router';
+import { Answer, Implementation, Question } from '@src/app/models';
 import { Controller } from '@src/app/services/control/Controller.service';
+import { ErrorTranslator } from '@src/app/services/ui/ErrorTranslator.service';
 
 @Component({
   selector: 'app-implementation-page',
@@ -10,11 +12,23 @@ import { Controller } from '@src/app/services/control/Controller.service';
 export class ImplementationPageComponent implements OnInit {
 
   implementation: Implementation;
+  id: string;
+  files: any[] = [];
+  questions: Question[] = [];
+  answers: Answer[] = [];
 
-  constructor(private controller: Controller) { }
+  constructor(private controller: Controller, private route: ActivatedRoute, private translator: ErrorTranslator) { 
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit(): void {
-    
+    this.controller.draftImplementation(this.id)
+      .then(impl => {this.implementation = <Implementation> impl})
+      .catch( err => { console.log(this.translator.translate(err)); } );
+
+    this.controller.getQuestions(this.id)
+      .then(qstns => {this.questions = qstns; qstns.map(q =>  {this.answers.push(new Answer(null,q.id, q.question, null, null))} )})
+      .catch( err => { console.log(this.translator.translate(err)); } );
   }
 
   statusFormat(completed: boolean){
@@ -23,6 +37,10 @@ export class ImplementationPageComponent implements OnInit {
 
   getRange(topLimit: number){
     return [...Array(topLimit).keys()].map(element => {element + 1});
+  }
+
+  onsubmit(){
+
   }
 
 }
