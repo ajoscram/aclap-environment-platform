d1aba7162774f94f7000a6bdae02a8a06900ad8a
ajoscram <ajoscram@gmail.com>
2021-02-09 00:38:48 -0600

    Fixed final merge conflict on MockDatabase

--- aclap/src/app/services/database/MockDatabase.service.ts
+++ aclap/src/app/services/database/MockDatabase.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { Module, IModule, DisciplineMetadata, Section, ISection, File, IFile, User, Administrator, Educator, Discipline, Subject, ImageSection, TitleSection, TitleSectionSize, ParagraphSection, ActivitySection, Question, Score, YoutubeVideoSection, IParagraphSection } from '@src/app/models';
-import ControlModule from '../../modules/control/control.module';
-import { SectionFactory } from './sections/SectionFactory.service';
-=======
 import { Module, DisciplineMetadata, Section, ISection, File, IFile, User, Administrator, Educator, Discipline, Subject, ImageSection, TitleSection, TitleSectionSize, ParagraphSection, ActivitySection, Question, IQuestion, Score, YoutubeVideoSection, IParagraphSection, IDisciplineMetadata, IUser, Implementable, Event, IImplementable, EducatorRequest, Implementation, Location, EducatorRequestState, Answer, IAnswer, IEducatorRequest, IImplementation, Ally, IAlly } from '../../models';
 import ControlModule from '../../modules/control/control.module';
 import { Factory } from './factory/Factory.service';
->>>>>>> develop
 import { Database, DatabaseError } from './Database.service';
 import { MockAuthenticator } from '../authentication/mock/MockAuthenticator.service';
 
@@ -19,12 +13,8 @@ export class MockDatabase implements Database{
     private ids: number = 0;
     private disciplineMetadata: DisciplineMetadata;
     private users: User[];
-<<<<<<< HEAD
-    private modules: Module[];
-=======
     private requests: EducatorRequest[];
     private implementables: Implementable[];
->>>>>>> develop
     private sections: Section[];
     private files: File[];
     private questions: Question[];
@@ -37,11 +27,7 @@ export class MockDatabase implements Database{
         return ''+this.ids++;
     }
     
-<<<<<<< HEAD
-    constructor(private sectionFactory: SectionFactory){
-=======
     constructor(private factory: Factory){
->>>>>>> develop
         this.disciplineMetadata = new DisciplineMetadata(
             //subjects
             [ new Subject('Estudios Sociales', '#585FC2'), new Subject('Cívica', '#019CF6'), new Subject('Ciencias', '#53A23C')],
@@ -62,25 +48,6 @@ export class MockDatabase implements Database{
             new EducatorRequest(this.nextId, 'Ayy', 'Lmao', 'ayy@lmao.com', '12345678', new Location('Area 51', 40, 50), new Date(), 'Marte', new Date(), EducatorRequestState.PENDING)
         ];
 
-<<<<<<< HEAD
-        this.sections = [
-            new TitleSection(''+this.ids++, 0, TitleSectionSize.H1, 'Título Grande'),
-            new ImageSection(''+this.ids++, 1, 'Pie de foto', 'https://media.nationalgeographic.org/assets/photos/000/284/28446.jpg', 'Referencia'),
-            new TitleSection(''+this.ids++, 2, TitleSectionSize.H2, 'Subtítulo'),
-            new ParagraphSection(''+this.ids++, 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-            new ActivitySection(''+this.ids++, 4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 30, 'Herramientas listadas acá',[
-                this.generateQuestion('¿Esto es una pregunta?'),
-                this.generateQuestion('¿Cómo se sintió al leer esa pregunta?'),
-                this.generateQuestion('¿Y al leer esa otra?'),
-            ]),
-            new TitleSection(''+this.ids++, 5, TitleSectionSize.H2, 'Otro subtítulo'),
-            new ParagraphSection(''+this.ids++, 6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-            new YoutubeVideoSection(''+this.ids++, 7, 'https://www.youtube.com/watch?v=XqZsoesa55w')
-        ];
-
-        this.files = [
-            new File(''+this.ids++, 'https://www.cs.ubc.ca/~gregor/teaching/papers/4+1view-architecture.pdf', 'PDF Ejemplo', new Date(), 114688)
-=======
         this.implementables = [
             new Module(this.nextId, 'MI ENTORNO', '#FAB521', 'https://ecosistemas.ovacen.com/wp-content/uploads/2018/02/ecosistema-manglar.jpg', 'https://ecosistemas.ovacen.com/wp-content/uploads/2018/02/ecosistema-manglar.jpg', '0', 'Admin', 'McUsername', 'Entre 5 y 10', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean venenatis arcu eget placerat semper. Duis ac elementum orci, eu hendrerit ex. Vestibulum sit amet lobortis ligula. Nunc vel lectus faucibus, fringilla metus eu, cursus tortor. Aliquam sodales sodales nisl nec egestas. Fusce a neque vel velit egestas maximus. Aenean at turpis sit amet nibh laoreet iaculis. Pellentesque egestas tortor quis viverra viverra. Maecenas non lacinia neque, sit amet vehicula libero.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean venenatis arcu eget placerat semper. Duis ac elementum orci, eu hendrerit ex. Vestibulum sit amet lobortis ligula. Nunc vel lectus faucibus, fringilla metus eu, cursus tortor. Aliquam sodales sodales nisl nec egestas. Fusce a neque vel velit egestas maximus. Aenean at turpis sit amet nibh laoreet iaculis. Pellentesque egestas tortor quis viverra viverra. Maecenas non lacinia neque, sit amet vehicula libero.', [
                 new Discipline(this.disciplineMetadata.subjects[0], '2do Año', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus semper elit et lobortis. In interdum velit at odio tristique lacinia. Aenean egestas, augue eu.'),
@@ -133,7 +100,6 @@ export class MockDatabase implements Database{
         ];
         this.evidence = [
             new File(this.nextId, 'https://www.cs.ubc.ca/~gregor/teaching/papers/4+1view-architecture.pdf', 'PDF_Ejemplo.pdf', new Date(), 114688)
->>>>>>> develop
         ];
 
         this.allies = [
@@ -185,38 +151,6 @@ export class MockDatabase implements Database{
         this.requests.push(request_);
         return request_;
     }
-<<<<<<< HEAD
-    
-    async addModule(module: IModule): Promise<Module>{
-        const module_: Module = new Module(
-            ''+this.ids++,
-            module.name,
-            module.$imageUrl,
-            module.publisherId,
-            module.publisherName,
-            module.publisherLastname,
-            module.recommendedAge,
-            module.objectives,
-            module.requirements,
-            module.disciplines
-        );
-        this.modules.push(module_);
-        return module_;
-    }
-    
-    async updateModule(id: string, module: IModule): Promise<Module>{
-        for(let module_ of this.modules){
-            if(module_.id === id){
-                module_.name = module.name;
-                module_.publisherId = module.publisherId;
-                module_.publisherName = module.publisherName;
-                module_.publisherLastname = module.publisherLastname;
-                module_.recommendedAge = module.recommendedAge;
-                module_.objectives = module.objectives;
-                module_.requirements = module.requirements;
-                module_.disciplines = module.disciplines;
-                return module_;
-=======
 
     async getEducatorRequests(): Promise<EducatorRequest[]>{
         const requests: EducatorRequest[] = [];
@@ -231,7 +165,6 @@ export class MockDatabase implements Database{
             if(request.id === id){
                 request.state = state;
                 return request;
->>>>>>> develop
             }
         }
         throw new Error(DatabaseError.IMPLEMENTABLE_NOT_FOUND);
@@ -260,45 +193,6 @@ export class MockDatabase implements Database{
         throw new Error(DatabaseError.IMPLEMENTABLE_NOT_FOUND);
     }
     
-<<<<<<< HEAD
-    async deleteModule(id: string): Promise<Module>{
-        for(let i = 0; i < this.modules.length; i++)
-            if(this.modules[i].id === id)
-                return this.modules.splice(i, 1)[0];
-        throw new Error(DatabaseError.MODULE_NOT_FOUND);
-    }
-    
-    async getDisciplineMetadata(): Promise<DisciplineMetadata>{
-        return this.disciplineMetadata;
-    }
-    
-    private getSection(sectionId: string): Section{
-        for(let section of this.sections)
-            if(section.id === sectionId)
-                return section;
-        throw new Error(DatabaseError.SECTION_NOT_FOUND);
-    }
-
-    async getSections(moduleId: string): Promise<Section[]>{
-        this.getModule(moduleId);//checking for module existance
-        return this.sections;
-    }
-    
-    async addSection(moduleId: string, section: ISection): Promise<Section>{
-        this.getModule(moduleId);//checking for module existance
-        const section_: Section = this.sectionFactory.getSection(section);
-        section_.id = ''+this.ids++;
-        this.sections.push(section_);
-        return section_;
-    }
-    
-    async updateSection(moduleId: string, sectionId: string, section: ISection): Promise<Section>{
-        this.getModule(moduleId);//checking for module existance
-        for(let i = 0; i < this.sections.length; i++){
-            if(this.sections[i].id === sectionId){
-                const section_: Section = this.sectionFactory.getSection(section);
-                section_.id = this.sections[i].id;
-=======
     async addImplementable(implementable: IImplementable): Promise<Implementable>{
         const implementable_: Implementable = this.factory.getImplementable(this.nextId, implementable);
         this.implementables.push(implementable_);
@@ -344,7 +238,6 @@ export class MockDatabase implements Database{
         for(let i = 0; i < this.sections.length; i++){
             if(this.sections[i].id === sectionId){
                 const section_: Section = this.factory.getSection(sectionId, section);
->>>>>>> develop
                 this.sections.splice(i, 1, section_);
                 return section_;
             }
@@ -352,31 +245,14 @@ export class MockDatabase implements Database{
         throw new Error(DatabaseError.SECTION_NOT_FOUND);
     }
     
-<<<<<<< HEAD
-    async deleteSection(moduleId: string, sectionId: string): Promise<Section>{
-        this.getModule(moduleId);//checking for module existance
-=======
     async deleteSection(implementableId: string, sectionId: string): Promise<Section>{
         this.getImplementable(implementableId);//checking for implementable existance
->>>>>>> develop
         for(let i = 0; i < this.sections.length; i++)
             if(this.sections[i].id === sectionId)
                 return this.sections.splice(i, 1)[0];
         throw new Error(DatabaseError.SECTION_NOT_FOUND);
     }
     
-<<<<<<< HEAD
-    async getFiles(moduleId: string, sectionId: string): Promise<File[]>{
-        this.getModule(moduleId);//checking for module existance
-        this.getSection(sectionId);//checking for section existance
-        return this.files;
-    }
-    
-    async addFile(moduleId: string, sectionId: string, file: IFile): Promise<File>{
-        this.getModule(moduleId);//checking for module existance
-        this.getSection(sectionId);//checking for section existance
-        const file_: File = new File(''+this.ids++, file.url, file.name, file.uploaded, file.bytes);
-=======
     async getFiles(implementableId: string): Promise<File[]>{
         this.getImplementable(implementableId);//checking for implementable existance
         return this.files;
@@ -385,20 +261,10 @@ export class MockDatabase implements Database{
     async addFile(implementableId: string, file: IFile): Promise<File>{
         this.getImplementable(implementableId);//checking for implementable existance
         const file_: File = this.factory.getFile(this.nextId, file);
->>>>>>> develop
         this.files.push(file_);
         return file_;
     }
     
-<<<<<<< HEAD
-    async deleteFile(moduleId: string, sectionId: string, fileId: string): Promise<File>{
-        this.getModule(moduleId);//checking for module existance
-        this.getSection(sectionId);//checking for section existance
-        for(let i = 0; i < this.files.length; i++)
-            if(this.files[i].id === fileId)
-                return this.files.splice(i, 1)[0];
-        throw new Error(DatabaseError.MODULE_NOT_FOUND);
-=======
     async deleteFile(implementableId: string, fileId: string): Promise<File>{
         this.getImplementable(implementableId);//checking for implementable existance
         for(let i = 0; i < this.files.length; i++)
@@ -579,6 +445,5 @@ export class MockDatabase implements Database{
             if(this.allies[i].id == id)
                 return this.allies.splice(i, 1)[0];
         throw new Error(DatabaseError.ALLY_NOT_FOUND);
->>>>>>> develop
     }
 }
