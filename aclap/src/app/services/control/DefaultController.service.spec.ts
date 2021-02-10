12633af7c5b38c0bd5152cc87e831952e424af2b
ajoscram <ajoscram@gmail.com>
2021-02-10 01:41:39 -0600

    Fixed the DefaultController spec file's merge conflicts.

--- aclap/src/app/services/control/DefaultController.service.spec.ts
+++ aclap/src/app/services/control/DefaultController.service.spec.ts
@@ -2,26 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import ControlModule from '@src/app/modules/control/control.module';
 import { Role } from '../authentication/Session.model';
 import { Authenticator } from '../authentication/Authenticator.service';
-<<<<<<< HEAD
-import { MockAuthenticator } from '../authentication/MockAuthenticator.service';
-=======
 import { MockAuthenticator } from '../authentication/mock/MockAuthenticator.service';
->>>>>>> develop
 import { Database } from '../database/Database.service';
 import { MockDatabase } from '../database/MockDatabase.service';
 import { Storage } from '../storage/Storage.service';
 import { MockStorage } from '../storage/MockStorage.service';
 import { Controller } from "./Controller.service";
 import { DefaultController } from './DefaultController.service';
-<<<<<<< HEAD
-import { correctPath as IMAGE_PATH } from '../control/pathfinding/split/testing';
-import { Discipline, DisciplineMetadata, IModule, IParagraphSection, Module, Section, Subject, File } from '../../models';
-
-describe('DefaultController', () => {
-
-    let stubIModule: IModule;
-    let stubISection: IParagraphSection;
-=======
 import { DisciplineMetadata, IModule, IParagraphSection, Module, Section, ISection, File, Implementable, Event, IEducatorRequest, User, EducatorRequest, EducatorRequestState, IImplementation, Implementation, Score, IAnswer, IQuestion, Question, Answer, IAlly, Ally } from '../../models';
 import { Factory } from '../database/factory/Factory.service';
 import { Session } from '../authentication/Session.model';
@@ -103,7 +90,6 @@ describe('DefaultController', () => {
     //This represents a file, because MockStorage actually ignores it altogether.
     const STUB_IFILE: any = {};
     
->>>>>>> develop
     let controller: Controller;
 
     beforeEach(async () => {
@@ -113,40 +99,12 @@ describe('DefaultController', () => {
                 { provide: Authenticator, useClass: MockAuthenticator },
                 { provide: Database, useClass: MockDatabase },
                 { provide: Storage, useClass: MockStorage },
-<<<<<<< HEAD
-                { provide: Controller, useClass: DefaultController }
-            ] 
-        });
-        controller = TestBed.inject(Controller);
-        await controller.login(MockAuthenticator.ADMIN_USERNAME, MockAuthenticator.PASSWORD, Role.ADMINISTRATOR);
-        stubIModule = {
-            name: 'name',
-            $imageUrl: IMAGE_PATH,
-            publisherId: 'publisherId',
-            publisherName: 'publisherName',
-            publisherLastname: 'publisherLastname',
-            recommendedAge: 4,
-            objectives: [ 'first objective', 'second objecive' ],
-            requirements: [ 'first requirement', 'second requirement' ],
-            disciplines: [ 
-                new Discipline(
-                    new Subject('Estudios Sociales', '#585FC2'),
-                    '1er Año', 'Eje temático'
-                )
-            ]
-        };
-        stubISection  = {
-            index: 0,
-            text: 'text'
-        };
-=======
                 { provide: Controller, useClass: DefaultController },
                 Factory
             ] 
         });
         controller = TestBed.inject(Controller);
         await controller.login(MockAuthenticator.ADMIN_USERNAME, MockAuthenticator.PASSWORD);
->>>>>>> develop
     });
 
 
@@ -154,11 +112,7 @@ describe('DefaultController', () => {
         //an administrator user is logged in on the before each, so they must be logged out
         await controller.logout();
         await expectAsync(
-<<<<<<< HEAD
-            controller.login(MockAuthenticator.ADMIN_USERNAME, MockAuthenticator.PASSWORD, Role.ADMINISTRATOR)
-=======
             controller.login(MockAuthenticator.ADMIN_USERNAME, MockAuthenticator.PASSWORD)
->>>>>>> develop
         ).toBeResolved();
     });
 
@@ -167,17 +121,6 @@ describe('DefaultController', () => {
         await expectAsync(controller.logout()).toBeResolved();
     });
 
-<<<<<<< HEAD
-    it('addModule(): adds a module', async () => {
-        const module: Module = await controller.addModule(stubIModule);
-        expect(module).toBeTruthy();
-    });
-
-    it('getModule(): gets a module given the correct id', async () => {
-        const added: Module = await controller.addModule(stubIModule);
-        const gotten: Module = await controller.getModule(added.id);
-        expect(gotten).toBeTruthy();
-=======
     it('getUser(): returns the current session\'s user', async () => {
         //an administrator user is logged in on the before each
         const user: User = await controller.getUser();
@@ -227,7 +170,6 @@ describe('DefaultController', () => {
         const request: EducatorRequest = await controller.addEducatorRequest(STUB_IEDUCATORREQUEST);
         const denied: EducatorRequest = await controller.denyEducatorRequest(request.id);
         expect(denied.state).toBe(EducatorRequestState.DENIED);
->>>>>>> develop
     });
 
     it('getModules(): gets the list of all available modules', async () => {
@@ -235,17 +177,6 @@ describe('DefaultController', () => {
         expect(modules).toBeTruthy();
     });
 
-<<<<<<< HEAD
-    it('updateModule(): updates an existing module', async () => {
-        const added: Module = await controller.addModule(stubIModule);
-        const updated: Module = await controller.updateModule(added.id, stubIModule);
-        expect(updated).toBeTruthy();
-    });
-
-    it('deleteModule(): ', async () => {
-        const added: Module = await controller.addModule(stubIModule);
-        const deleted: Module = await controller.deleteModule(added.id);
-=======
     it('getEvents(): gets the list of all available events', async () => {
         const events: Event[] = await controller.getEvents();
         expect(events).toBeTruthy();
@@ -271,7 +202,6 @@ describe('DefaultController', () => {
     it('deleteImplementable(): deletes an implmentable given the correct id', async () => {
         const added: Implementable = await controller.addImplementable(STUB_IMODULE);
         const deleted: Implementable = await controller.deleteImplementable(added.id);
->>>>>>> develop
         expect(deleted).toBeTruthy();
     });
 
@@ -281,31 +211,6 @@ describe('DefaultController', () => {
     });
 
     it('addSection(): adds a section', async () => {
-<<<<<<< HEAD
-        const module: Module = await controller.addModule(stubIModule);
-        const section: Section = await controller.addSection(module.id, stubISection);
-        expect(section).toBeTruthy();
-    });
-
-    it('getSections(): gets a module\'s list of sections', async () => {
-        const module: Module = await controller.addModule(stubIModule);
-        await controller.addSection(module.id, stubISection);
-        const sections: Section[] = await controller.getSections(module.id);
-        expect(sections).toBeTruthy();
-    });
-
-    it('updateSection(): updates an existing session', async () => {
-        const module: Module = await controller.addModule(stubIModule);
-        const added: Section = await controller.addSection(module.id, stubISection);
-        const updated: Section = await controller.updateSection(module.id, added.id, stubISection);
-        expect(updated).toBeTruthy();
-    });
-
-    it('deleteSection(): deletes an existing section', async () => {
-        const module: Module = await controller.addModule(stubIModule);
-        const added: Section = await controller.addSection(module.id, stubISection);
-        const deleted: Section = await controller.deleteSection(module.id, added.id);
-=======
         const implementable: Implementable = await controller.addImplementable(STUB_IMODULE);
         const section: Section = await controller.addSection(implementable.id, STUB_ISECTION);
         expect(section).toBeTruthy();
@@ -337,24 +242,10 @@ describe('DefaultController', () => {
         const implementable: Implementable = await controller.addImplementable(STUB_IMODULE);
         const added: Section = await controller.addSection(implementable.id, STUB_ISECTION);
         const deleted: Section = await controller.deleteSection(implementable.id, added.id);
->>>>>>> develop
         expect(deleted).toBeTruthy();
     });
 
     it('addFile(): adds a file', async () => {
-<<<<<<< HEAD
-        const module: Module = await controller.addModule(stubIModule);
-        const section: Section = await controller.addSection(module.id, stubISection);
-        const file: File = await controller.addFile(module.id, section.id, IMAGE_PATH);
-        expect(file).toBeTruthy();
-    });
-
-    it('getFiles(): gets a section\'s files', async () => {
-        const module: Module = await controller.addModule(stubIModule);
-        const section: Section = await controller.addSection(module.id, stubISection);
-        await controller.addFile(module.id, section.id, IMAGE_PATH);
-        const files: File[] = await controller.getFiles(module.id, section.id);
-=======
         const implementable: Implementable = await controller.addImplementable(STUB_IMODULE);
         const file: File = await controller.addFile(implementable.id, STUB_IFILE);
         expect(file).toBeTruthy();
@@ -364,18 +255,10 @@ describe('DefaultController', () => {
         const implementable: Implementable = await controller.addImplementable(STUB_IMODULE);
         await controller.addFile(implementable.id, STUB_IFILE);
         const files: File[] = await controller.getFiles(implementable.id);
->>>>>>> develop
         expect(files).toBeTruthy();
     });
 
     it('deleteFile(): deletes an existing file', async () => {
-<<<<<<< HEAD
-        const module: Module = await controller.addModule(stubIModule);
-        const section: Section = await controller.addSection(module.id, stubISection);
-        const added: File = await controller.addFile(module.id, section.id, IMAGE_PATH);
-        const deleted: File = await controller.deleteFile(module.id, section.id, added);
-        expect(deleted).toBeTruthy();
-=======
         const implementable: Implementable = await controller.addImplementable(STUB_IMODULE);
         const added: File = await controller.addFile(implementable.id, STUB_IFILE);
         const deleted: File = await controller.deleteFile(implementable.id, added.id);
@@ -585,7 +468,6 @@ describe('DefaultController', () => {
         const deleted: Ally = await controller.deleteAlly(added.id);
         expect(deleted).toBeTruthy();
         expect(deleted.id).toBe(added.id);
->>>>>>> develop
     });
 
     /*
